425fb41f96be3dbb115616308c7438a3d5e3d0f9
dayofni <trashemail.dcook@gmail.com>
2024-04-29 20:12:31 +0930

    split everything to individual files

---
+++ ilo/awen.py
@@ -0,0 +1,16 @@
+
+from discord import Message, Client, PartialEmoji, Member
+
+MESSAGES = {}
+
+async def awen_a(client: Client, user: Member, reaction: PartialEmoji, message: Message, config: dict = {}):
+    
+    # Check that the react is a STOP
+    
+    # Add to MESSAGES[message_id] and check if the message has been sufficiently STOP'd
+    
+    # if so, delete, then delete it from MESSAGES
+    
+    
+    
+    ...
--- ilo/bot.py
+++ ilo/bot.py
@@ -1,18 +1,41 @@
 
 import discord
 
+from discord import Message, RawReactionActionEvent
+
+from ilo.awen import awen_a
+from ilo.musi import mu_a
+        
+ON_MESSAGE = [mu_a]
+ON_REACT   = [awen_a]
+
+CONFIG = {
+    
+    "REACT_IGNORE_DAYS":   7,
+    "STOP_MESSAGE_AMOUNT": 5
+}
+
 class ilo(discord.Client):
     
     async def on_ready(self):
         print(f'Logged on as {self.user}!')
 
-    async def on_message(self, message):
+    async def on_message(self, message: Message):
         
         print(f'Message from {message.author}: {message.content}')
         
-        if message.author == self.user:
+        for func in ON_MESSAGE:
+            await func(self, message, config=CONFIG)
+    
+    async def on_raw_reaction_add(self, payload: RawReactionActionEvent):
+        
+        user, reaction = payload.member, payload.emoji
+        
+        message = self.get_message(payload.message_id)
+        
+        if not message:
+            print("Couldn't find attached message.")
             return
         
-        if any(["".join(list(sorted(set(i.strip("`~!@#$%^&*()_-+={[}]|\\:;\"'<,>.?/"))))) == "mu" for i in message.content.lower().split()]):
-            
-            await message.channel.send('mu a!')
+        for func in ON_REACT:
+            await func(self, user, reaction, message, config=CONFIG)
