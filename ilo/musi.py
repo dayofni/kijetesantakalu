27959a6ef1d70f7578b713540dda07840fe7223b
dayofni <trashemail.dcook@gmail.com>
2024-05-04 17:48:09 +0930

    simplified the """regex"""

--- ilo/musi.py
+++ ilo/musi.py
@@ -3,5 +3,5 @@ from discord import Message, Client
 
 async def mu_a(client: Client, message: Message, config: dict = {}):
         
-    if any(["".join(list(sorted(set(i.strip("`~!@#$%^&*()_-+={[}]|\\:;\"'<,>.?/"))))) == "mu" for i in message.content.lower().split()]):        
+    if any(["".join(sorted(set(i.strip("`~!@#$%^&*()_-+={[}]|\\:;\"'<,>.?/")))) == "mu" for i in message.content.lower().split()]):        
         await message.channel.send('mu a!')
